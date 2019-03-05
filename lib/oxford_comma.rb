097fafa929ad3db541124ac675c1119d5f1e8aa5
Matthew Wilson <wilsonmatt219@gmail.com>
2019-03-05 23:54:21 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.length == 1 
+    "#{array[0]}"
+    elsif array.length == 2 
+    array.join(" and " ) 
+  else array.length == 3
+    last = array.pop
+    return "#{array.join(", ")}, and #{last}"
+  end
 end
